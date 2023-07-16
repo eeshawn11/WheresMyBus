f8a8e8ed632c7956e705f5fd17d3c48b7ca923c9
eeshawn <115083646+eeshawn11@users.noreply.github.com>
2023-07-16 22:58:59 +0800

    update
    - Add timeout to request
    - Try except to catch request errors
    - Sort results by ETA

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ stop_url = "http://datamall2.mytransport.sg/ltaodataservice/BusStops"
 headers = {
     "AccountKey": st.secrets["lta_token"]
 }
+TIMEOUT = (5, 15)
 
-bus_dict = {
+response_dict = {
     "Load": {
         "SEA": "with seats.",
         "SDA": "with standing only.",
@@ -27,74 +28,95 @@ def get_stops():
     bus_stops = {}
     
     while True:
-        response = requests.get(stop_url+"?$skip="+str(skip_operator), headers=headers)
-        results = response.json()
+        try:
+            response = requests.get(stop_url+"?$skip="+str(skip_operator), headers=headers, timeout=TIMEOUT)
+            response.raise_for_status()
+            data = response.json()
 
-        if response.status_code == 200:
-            if results["value"]:
-                for stop in results["value"]:
-                    bus_stops[stop["BusStopCode"]] = f"{stop['Description']} along {stop['RoadName']}"
+            if data["value"]:
+                for stop in data["value"]:
+                    bus_stops[stop["BusStopCode"]] = f"{stop['Description'].title()} along {stop['RoadName']}"
             else:
                 break
-        else:
-            print("Error")
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            st.error("Sorry, an error occurred. Please try again.")
+            break
+        except requests.exceptions.JSONDecodeError as e:
+            print(e)
+            st.error("Sorry, an error occurred. Please try again.")    
+            break
         
         skip_operator += 500
 
     print(f"{len(bus_stops.keys())} stops retrieved.")
     return bus_stops
 
-def read_response(service):
-    results = []
-    for bus in ["NextBus", "NextBus2", "NextBus3"]:
-        if service[bus]["EstimatedArrival"]:
-            # get eta
-            bus_eta = dt.strptime(service[bus]['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S%z") - dt.now().astimezone()
-            bus_eta_mins = round(bus_eta.total_seconds() / 60)
-            # check bus type and load
-            bus_type = bus_dict["Type"][service[bus]['Type']]
-            bus_load = bus_dict["Load"][service[bus]['Load']]
-            results.append([bus_eta_mins, bus_type, bus_load])
-        else:
-            results.append([None, None, None])
-    return results
+def read_response(data):
+    results = {}
+    for service in data["Services"]:
+        service_no = service["ServiceNo"]
+        results[service_no] = {
+            "ETA": [],
+            "Buses": []
+        }
+        for bus in ["NextBus", "NextBus2", "NextBus3"]:
+            if service[bus]["EstimatedArrival"]:
+                # get eta
+                bus_eta = dt.strptime(service[bus]['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S%z") - dt.now().astimezone()
+                bus_eta_mins = round(bus_eta.total_seconds() / 60)
+                # check bus type and load
+                bus_info = f"{response_dict['Type'][service[bus]['Type']]} {response_dict['Load'][service[bus]['Load']]}"
+                if bus_eta_mins >= 0:
+                    results[service_no]["ETA"].append(bus_eta_mins)
+                results[service_no]["Buses"].append([bus_eta_mins, bus_info])
+            else:
+                results[service_no]["Buses"].append([None, None, None])
+
+    # sort by ETA
+    sorted_results = sorted(results.items(), key=lambda x:x[1]["ETA"][0])
+    return dict(sorted_results)
 
 @st.cache_data(ttl=15, show_spinner=False)
 def get_arrivals(bus_stop):
-    response = requests.get(arrival_url+bus_stop, headers=headers)
+    try:
+        response = requests.get(arrival_url+bus_stop, headers=headers, timeout=TIMEOUT)
+        response.raise_for_status()
+        data = response.json()
 
-    if response.status_code == 200:
-        results = response.json()
-        for bus_service in results["Services"]:
-            results = read_response(bus_service)
-            eta_list = [eta for eta, *_ in results if eta is not None and eta >= 0]
-            # display results
-            with st.expander(f"**{bus_service['ServiceNo']}** in {eta_list[0]} mins"):
-                    for result in results:
-                        if result[0] is None:
-                            pass
-                        elif result[0] == 0:
-                            st.markdown(f"Bus is here! {result[1]} {result[2]}")
-                        elif result[0] < 0:
-                            st.markdown(f"Oops, you just missed the bus.")
-                        elif result[0] > 0:
-                            st.markdown(f"**{result[0]}** mins. {result[1]} {result[2]}")
-    else:
-        return "Error"
+        results2 = read_response(data)
+        for service_no, details in results2.items():
+            with st.expander(f"**{service_no}** in {details['ETA'][0]} mins"):
+                for bus in details["Buses"]:
+                    if bus[0] is None:
+                        pass # add check if last service
+                    elif bus[0] == 0:
+                        st.markdown(f"Bus is here! {bus[1]}")
+                    elif bus[0] < 0:
+                        st.markdown("Oops, you just missed this bus.")
+                    elif bus[0] > 0:
+                        st.markdown(f"**{bus[0]}** mins. {bus[1]}")
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        st.error("Sorry, an error occurred. Please try again.")
+    except requests.exceptions.JSONDecodeError as e:
+        print(e)
+        st.error("Sorry, an error occurred. Please try again.")    
 
 bus_stops = get_stops()
 
 st.title("Where's My Bus? :bus:")
 st.markdown("Stalk your bus, made possible by the [Land Transport Authority](https://datamall.lta.gov.sg/content/datamall/en.html) of Singapore.")
+my_stop = st.text_input("Which bus stop?", max_chars=5, key="my_stop")
+results_placeholder = st.empty()
 
-my_stop = st.text_input("Which bus stop?", max_chars=5)
-
-if my_stop:
-    if my_stop.isdigit() and len(my_stop) == 5:
-        if my_stop in bus_stops:
-            st.header(f"{bus_stops[my_stop]}")
-            get_arrivals(my_stop)
+with results_placeholder.container():
+    if my_stop:
+        if my_stop.isdigit() and len(my_stop) == 5:
+            if my_stop in bus_stops:
+                st.header(f"{bus_stops[my_stop]} ({my_stop})")
+                get_arrivals(my_stop)
+            else:
+                st.error("Bus stop not found. Please try again.")
         else:
-            st.error("Bus stop not found. Please try again.")
-    else:
-        st.error("Please provide a 5 digit bus stop code.")
+            st.error("Please provide a 5 digit bus stop code.")
